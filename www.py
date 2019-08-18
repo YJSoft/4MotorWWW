adaa90896fc892914dc59ce84c5b5718fb9c28a5
YJSoft <YJSoft@users.noreply.github.com>
2019-08-18 18:05:35 +0900

    Update www.py

--- www.py
+++ www.py
@@ -1,11 +1,11 @@
 import traceback, struct
 from time import time
 from flask import Flask
-from car import setSpeed
-from car import setOutMode
+from car import FourMotor
 import RPi.GPIO as GPIO
 
 wwwapp = Flask(__name__)
+myCar = FourMotor()
 
 @wwwapp.route("/")
 def rootpage():
@@ -15,31 +15,26 @@ def rootpage():
 def cmd_move(type, speed):
     speed = int(speed)
     if type == "forward":
-        setSpeed(1,speed,1)
-        setSpeed(2,speed,1)
-        setSpeed(3,speed,0)
-        setSpeed(4,speed,0)
+        myCar.setSpeed(1,speed,1)
+        myCar.setSpeed(2,speed,1)
+        myCar.setSpeed(3,speed,0)
+        myCar.setSpeed(4,speed,0)
     elif type == "backward":
-        setSpeed(1,speed,0)
-        setSpeed(2,speed,0)
-        setSpeed(3,speed,1)
-        setSpeed(4,speed,1)
+        myCar.setSpeed(1,speed,0)
+        myCar.setSpeed(2,speed,0)
+        myCar.setSpeed(3,speed,1)
+        myCar.setSpeed(4,speed,1)
     elif type == "left":
-        setSpeed(1,speed,1)
-        setSpeed(2,speed,0)
-        setSpeed(3,speed,0)
-        setSpeed(4,speed,1)
+        myCar.setSpeed(1,speed,1)
+        myCar.setSpeed(2,speed,0)
+        myCar.setSpeed(3,speed,0)
+        myCar.setSpeed(4,speed,1)
     elif type == "right":
-        setSpeed(1,speed,0)
-        setSpeed(2,speed,1)
-        setSpeed(3,speed,1)
-        setSpeed(4,speed,0)
-    return "OK"
+        myCar.setSpeed(1,speed,0)
+        myCar.setSpeed(2,speed,1)
+        myCar.setSpeed(3,speed,1)
+        myCar.setSpeed(4,speed,0)
+    return "OK-Move"
 
 if __name__ == '__main__':
-    GPIO.setmode(GPIO.BCM)
-    
-    for i in range(1, 5):
-        setOutMode(i)
-    
     wwwapp.run(host='0.0.0.0', port=8088)
